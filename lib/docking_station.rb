3dc76fae34b8f44a142f5cf51fbf5087ca6c5554
Deon Tan <deonjtan@gmail.com>
2019-05-28 15:59:14 +0100

    [WIP] draft klass impl

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,44 +1,72 @@
+# module DockingStation
+#   DEFAULT_CAPACITY = 20
+#   @@self = DockingStation
+#   @@dot_class = :DockingStation
+#   @@dot_new = lambda do |capacity=DEFAULT_CAPACITY|
+#     dot_capacity = DEFAULT_CAPACITY
+#     dot_bikes = []
+#
+#     dot_dock = lambda do |bike|
+#       dot_bikes.push(bike)
+#     end
+#
+#     dot_release_bike = lambda do 
+#       dot_bikes.pop
+#     end
+#
+#     dispatch = lambda do |message|
+#
+#       case message
+#       when :dot_class?
+#         @@self::dot_class?
+#       when :dot_dock
+#         dot_dock
+#       when :dot_bikes
+#         dot_bikes
+#       when :dot_release_bike
+#         dot_release_bike
+#       else
+#         raise "Undefined method exception"
+#       end
+#
+#     end
+#
+#     dispatch
+#   end
+#
+#   def DockingStation::dot_new
+#     @@dot_new
+#   end
+#   def DockingStation::dot_class?
+#     @@dot_class
+#   end
+# end
+#
+require 'klass.rb'
 module DockingStation
-  DEFAULT_CAPACITY = 20
   @@self = DockingStation
-  @@dot_class = :DockingStation
-  @@dot_new = lambda do |capacity=DEFAULT_CAPACITY|
-    dot_capacity = DEFAULT_CAPACITY
-    dot_bikes = []
-
-    dot_dock = lambda do |bike|
-      dot_bikes.push(bike)
+  @@klass = Klass::dot_new(:DockingStation, [
+    :def_initialize, lambda do |zelf, capacity=20|
+      zelf[:at_capacity] = capacity
+      zelf[:at_bikes] = []
+      zelf
+    end,
+    :def_bikes, lambda do |zelf|
+      zelf[:at_bikes]
+    end,
+    :def_dock, lambda do |zelf, bike|
+      zelf[:at_bikes].push(bike)
+    end,
+    :def_release_bike, lambda do |zelf|
+      zelf[:at_bikes].pop
     end
-
-    dot_release_bike = lambda do 
-      dot_bikes.pop
-    end
-
-    dispatch = lambda do |message|
-
-      case message
-      when :dot_class?
-        @@self::dot_class?
-      when :dot_dock
-        dot_dock
-      when :dot_bikes
-        dot_bikes
-      when :dot_release_bike
-        dot_release_bike
-      else
-        raise "Undefined method exception"
-      end
-
-    end
-
-    dispatch
-  end
+  ])
 
   def DockingStation::dot_new
-    @@dot_new
+    @@klass[:dot_new]
   end
+
   def DockingStation::dot_class?
-    @@dot_class
+    @@klass[:dot_class?]
   end
 end
-
